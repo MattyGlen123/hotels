68bc159562a3379342d7a2c1645fbc78410a99f2
matty <matthewg1993@hotmail.co.uk>
2023-09-25 07:36:08 +0100

    Add classList tets

--- __tests__/classlist.test.ts
+++ __tests__/classlist.test.ts
@@ -1,13 +1,23 @@
 import { classList } from '@/util/classList';
 
-import { render, screen } from '@testing-library/react'
-
 import '@testing-library/jest-dom'
  
 describe('classList', () => {
   it('should return a valid string', () => {
     const res = classList(['123', '456']);
 
-    expect(res).toEqual('123 456 ')
+    expect(res).toEqual('123 456')
+  })
+
+  it('should return an empty string', () => {
+    const res = classList(['']);
+
+    expect(res).toEqual('')
+  })
+
+  it('should return a single string', () => {
+    const res = classList(['123']);
+
+    expect(res).toEqual('123')
   })
 })
--- src/util/classList.ts
+++ src/util/classList.ts
@@ -1,9 +1,9 @@
 export const classList = (classes: string[]) => {
   let concatenatedString = '';
 
-  classes.forEach(item => {
-    if (typeof item === 'string')
-      concatenatedString += `${item} `;
+  classes.forEach((item, i) => {
+    if (item && typeof item === 'string') 
+      concatenatedString += `${item}${classes.length === i + 1 ? '' : ' '}`;  
   })
 
   return concatenatedString;
