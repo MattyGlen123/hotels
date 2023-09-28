a0f31eb48b51b2d0f21dc62624135ec22307f6d8
matty <matthewg1993@hotmail.co.uk>
2023-09-28 21:05:53 +0100

    attach tests to components

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,10 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleDirectories: ["node_modules", "<rootDir>/", "**/*.test.{ts,tsx}"],
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  clearMocks: true
 };
 
 module.exports = createJestConfig(customJestConfig);
