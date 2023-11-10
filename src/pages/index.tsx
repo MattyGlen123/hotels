cf4cccd9b85a50c5347ba8fb7a51d563f285eccb
matty <matthewg1993@hotmail.co.uk>
2023-11-10 11:56:58 +0000

    Add button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
+'use client'
 
-import { Search } from "../components/search/search";
-import { CurrentWeather } from "@/components/current-weather/current-weather";
-import { Loading } from "@/components/loading/loading";
-import { useCurrentWeather } from "@/hooks/use-current-weather";
+import { Button } from '../components/button/button'
+// import { Loading } from '@/components/loading/loading'
 
 export default function Home() {
-  const { isError, weatherData, isLoading, error } = useCurrentWeather();
+  // const { isError, weatherData, isLoading, error } = useCurrentWeather()
 
   return (
-    <main className="flex min-h-screen flex-col py-12 px-4 bg-sky-950 text-slate-50	">
-      <Search />
+    <main className="">
+      <Button label="Book Now" price="£199.99" />
 
-      {isLoading && <Loading />}
+      {/* {isLoading && <Loading />} */}
 
-      {isError && <p>{error?.message}</p>}
-
-      {weatherData && <CurrentWeather {...weatherData} />}
+      {/* {isError && <p>{error?.message}</p>} */}
     </main>
-  );
+  )
 }
